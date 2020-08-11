e660d9a20f3a14e75fad780e13695617be2e359a
Marchael Acode <m.acode@outlook.com>
2020-08-11 14:31:18 -0700

    Completed feature #1

--- README.md
+++ README.md
@@ -4,21 +4,21 @@
 
 1. Feature #1: Display images
 
-- Estimate of time needed to complete: 1 hour
+- Estimate of time needed to complete: 45 minutes
 - Start time: 2:00 PM
-- Finish time:
-- Actual time needed to complete:
+- Finish time: 2:30 PM
+- Actual time needed to complete: 30 mintues
 
 2. Feature #2: Filter images
 
-- Estimate of time needed to complete: 1 hour
+- Estimate of time needed to complete: 45 minutes
 - Start time:
 - Finish time:
 - Actual time needed to complete:
 
 3. Feature #3: Style the application
 
-- Estimate of time needed to complete: 1 hour
+- Estimate of time needed to complete: 45 minutes
 - Start time:
 - Finish time:
 - Actual time needed to complete:
--- js/app.js
+++ js/app.js
@@ -1,2 +1,35 @@
 'use strict';
 
+const imageArray = [];
+
+function Image (url, title, desc, keyword, horns) {
+  this.url = url;
+  this.title = title;
+  this.desc = desc;
+  this.keyword = keyword;
+  this.horns = horns;
+
+  imageArray.push(this);
+}
+
+Image.prototype.renderJQ = function () {
+  const $clonedSection = $('section:first-child').clone();
+
+  $clonedSection.find('h2').text(this.title);
+  $clonedSection.find('img').attr('src', this.url).attr('alt', this.title);
+  $clonedSection.find('p').text(this.desc);
+
+  $('main').append($clonedSection);
+};
+
+const makeImageInstances = jsonArr => {
+  jsonArr.forEach(indexObj => {
+    new Image (indexObj.image_url, indexObj.title, indexObj.description, indexObj.keyword, indexObj.horns);
+  });
+
+  imageArray.forEach(arrIndexVal => arrIndexVal.renderJQ());
+
+};
+
+$.get('data/page-1.json')
+  .then(makeImageInstances);
